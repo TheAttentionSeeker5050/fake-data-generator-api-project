846876cd47515a5622c4d680eec07a5d20025233
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-04-11 19:41:53 -0300

    created simple hello world template for running the app

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ping pong",
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	// prepare jsoniter response
+	router.GET("/api", func(c *gin.Context) {
+
+		var data = struct {
+			Name string `json:"name"`
+			Age  int    `json:"age"`
+		}{
+			Name: "John",
+			Age:  30,
+		}
+
+		var json = jsoniter.ConfigCompatibleWithStandardLibrary
+		json.Marshal(&data)
+		c.JSON(200, data)
+	})
+
+	// make hello world h1 tag on / in plain text
+	router.GET("/", func(c *gin.Context) {
+
+		// render h1 tag
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Hello World",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
